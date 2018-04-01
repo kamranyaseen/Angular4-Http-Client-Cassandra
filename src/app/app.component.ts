75a5cc240e504bf61fd2be805683ef5e64f0f186
Kamran Yaseen <kamranmyaseen@gmail.com>
2018-04-01 13:02:05 +0300

    Angular4 http client for Cassandra CRUD Operations
    Angular4 http client for Cassandra CRUD Operations

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+  title = 'Angular 4 with Spring boot';
+  description = 'Angular4-Client App';
+
+  constructor() { }
+
+}
